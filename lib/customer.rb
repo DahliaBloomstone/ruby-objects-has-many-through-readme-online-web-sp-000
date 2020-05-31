e1c9abe63342c6b61d121112e1a605accba0a6bc
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-05-31 20:03:10 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -13,8 +13,8 @@ def self.all #returns the contents of @@all
   @@all
 end
 
-def new_meal(waiter, total, tip=0)
-  Meal.new(waiter, self, total, tip)
+def new_meal(waiter, total, tip=0) #initializes a meal using the current Customer instance
+  Meal.new(waiter, self, total, tip) #provided a Waiter instance, a total, and a tip
 end
 
 def meals
